fd932443d9994633978871a3adbd6b35b8af0a76
noneisyourokk-ux <noneisyourokk@gmail.com>
2026-07-24 18:16:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,7 @@ logging.basicConfig(
 )
 
 def main():
-    if not config.BOT_TOKEN or config.BOT_TOKEN == "8683892422:AAFBEVW0vxbRxqpObQsyGWcQtb1-N6wB6DU":
-        raise ValueError("BOT_TOKEN is missing in environment variables!")
-
+    if not config.BOT_TOKEN or config.BOT_TOKEN == "8683892422:AAFBEVW0vxbRxqpObQsyGWcQtb1-N6wB6DU"
     app = ApplicationBuilder().token(config.BOT_TOKEN).build()
 
     # Handlers
